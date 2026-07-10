0f5be8446ff71cd131ee3dd571b557f0c39f4efc
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-07-10 15:11:34 -0300

    Release version 1.0.7

--- f10-lead-capture.php
+++ f10-lead-capture.php
@@ -3,7 +3,7 @@
  * Plugin Name: F10 Lead Capture
  * Plugin URI: https://github.com/jefersonMarques/wp_plugin_f10_software
  * Description: Capture WordPress leads, store them locally, and optionally send them to F10 Software and Brevo.
- * Version: 1.0.6
+ * Version: 1.0.7
  * Author: F10 Software
  * Author URI: https://f10.com.br/
  * License: GPLv2 or later
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('F10_LEAD_CAPTURE_VERSION', '1.0.6');
+define('F10_LEAD_CAPTURE_VERSION', '1.0.7');
 define('F10_LEAD_CAPTURE_FILE', __FILE__);
 define('F10_LEAD_CAPTURE_PATH', plugin_dir_path(__FILE__));
 define('F10_LEAD_CAPTURE_URL', plugin_dir_url(__FILE__));
